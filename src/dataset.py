ebccd492c2b4fcde486476677db3308ecdb2c6bc
dext01 <130887490+dext01@users.noreply.github.com>
2026-03-11 21:05:23 +0700

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,9 @@
 import torch
 import random
-
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+import os
 
 def get_transforms(img_size=128):
     return transforms.Compose([
@@ -11,7 +11,6 @@ def get_transforms(img_size=128):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-
 
 def get_dataloaders(data_path, batch_size=32, val_split=0.2, seed=42):
     transform = get_transforms()
@@ -33,3 +32,37 @@ def get_dataloaders(data_path, batch_size=32, val_split=0.2, seed=42):
     )
     val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=False, num_workers=2)
     return train_loader, val_loader, dataset.classes
+
+class KaggleTestDataset(Dataset):
+    def __init__(self, main_data_dir, test_dir, transform=None):
+        self.main_data_dir = main_data_dir
+        self.test_dir = test_dir
+        self.transform = transform
+        self.classes = sorted([d for d in os.listdir(main_data_dir) if os.path.isdir(os.path.join(main_data_dir, d))])
+        self.class_to_idx = {cls: idx for idx, cls in enumerate(self.classes)}
+        self.samples = []
+        for filename in os.listdir(test_dir):
+            if filename.lower().endswith(('.jpg', '.png')):
+                name = filename.split('.')[0]
+                if '_on_' in name:
+                    character = name.split('_on_')[0]
+                else:
+                    parts = name.split('_')
+                    if len(parts) >= 2 and parts[-1].isdigit():
+                        character = '_'.join(parts[:-1])
+                    else:
+                        character = name
+                if character in self.class_to_idx:
+                    img_path = os.path.join(test_dir, filename)
+                    label = self.class_to_idx[character]
+                    self.samples.append((img_path, label))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        img_path, label = self.samples[idx]
+        image = Image.open(img_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image, label
